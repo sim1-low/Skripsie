bfeba609b7d5eaac64d62caa4751fff815b2fcfa
sim1-low <simone.carstens1@gmail.com>
2024-10-15 17:24:47 +0200

    Update Quiz_1.jsx

--- s-app/src/Components/Quiz/Quiz_1.jsx
+++ s-app/src/Components/Quiz/Quiz_1.jsx
@@ -162,7 +162,7 @@ export const Quiz_1 = () => {
         } else{
             const currentDate = new Date();
             setQuizCompleted(true);
-            axios.post('https://skripsie-qjut.vercel.app/quizScore', {name:name, email:email, date:currentDate, quizid:"quiz1", score:correctAnswers})
+            axios.post('https://skripsie-backend.vercel.app//quizScore', {name:name, email:email, date:currentDate, quizid:"quiz1", score:correctAnswers})
             .then(result => {console.log(result)})
         }
     };
