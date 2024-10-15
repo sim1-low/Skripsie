143459151109469043714ed6eb0ff5991843c05d
sim1-low <simone.carstens1@gmail.com>
2024-10-15 17:39:13 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,6 +12,12 @@ app.use(cors({
      credentials: true
  }));
 
+app.use((req, res, next) => {
+       res.setHeader('Access-Control-Allow-Origin', 'https://skripsie.vercel.app'); // Replace with your frontend origin
+       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+       res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+       next();
+     });
 
 
 
