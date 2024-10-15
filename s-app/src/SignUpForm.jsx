7e6c7101909222c2dbcfd0e13fd33bb1d89b11f7
sim1-low <simone.carstens1@gmail.com>
2024-10-15 16:30:14 +0200

    Update SignUpForm.jsx

--- s-app/src/SignUpForm.jsx
+++ s-app/src/SignUpForm.jsx
@@ -13,7 +13,7 @@ function Signup(){
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        axios.post('http://skripsie-qjut.vercel.app/register', {name, email, password})
+        axios.post('https://skripsie-qjut.vercel.app/register', {name, email, password})
         .then(result => {console.log(result)
         navigate('/login')
         })
