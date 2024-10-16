5e9afb0a72ceebdc9d812fc34453be076d936d9f
sim1-low <simone.carstens1@gmail.com>
2024-10-16 15:14:07 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,16 +8,31 @@ const dotenv = require('dotenv').config();
 const app = express()
 app.use(express.json())
 app.use(cors({
-     origin: "https://skripsie.vercel.app",
-     credentials: true
- }));
+       origin: 'https://skripsie.vercel.app', // Allow requests from this origin
+       methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+       allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+     }));
 
-app.use((req, res, next) => {
-       res.setHeader('Access-Control-Allow-Origin', 'https://skripsie.vercel.app'); // Replace with your frontend origin
-       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-       res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-       next();
-     });
+//app.use(cors({
+ //    origin: "https://skripsie.vercel.app",
+ //    credentials: true
+ //}));
+
+//app.use((req, res, next) => {
+//       res.setHeader('Access-Control-Allow-Origin', 'https://skripsie.vercel.app'); // Replace with your frontend origin
+  //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+ //      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  //     next();
+  //   });
+
+
+
+
+
+
+     
+
+     // Your backend routes and logic go here...
 
 
 
